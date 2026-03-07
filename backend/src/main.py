46ffec96ccc1d1697a95ba2b31dc04b3959ad674
Kirill Gorskiy <kirillgorskiy@gmail.com>
2026-03-08 01:39:29 +0300

    Create simple docker-compose.yml with backend and postgres

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,17 +1,13 @@
-from fastapi import FastAPI, UploadFile, HTTPException
-from routers import recording_router, notification_router, suspicious_router, student_router
+from fastapi import FastAPI
+from .routers import recording_router, notification_router, suspicious_router, student_router
 
 app = FastAPI()
-
 
 app.include_router(recording_router.router)
 app.include_router(notification_router.router)
 app.include_router(suspicious_router.router)
 app.include_router(student_router.router)
+
 @app.get("/")
 async def root():
     return {"message": "API started"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
