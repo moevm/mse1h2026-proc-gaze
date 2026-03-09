516383767443ea6893c3966749ed305c1954e205
Berlet Maxim <berletmaximq@gmail.com>
2026-03-09 19:50:37 +0300

    feat: refactored code a bit

---
+++ tracking/src/gaze_mapper.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class GazeMapper:
+    def __init__(self):
+        pass
+
+    def project(self, gaze_vec: np.ndarray) -> np.ndarray:
+        pass
+    
+    def calibrate(self) -> None:
+        pass
+    
