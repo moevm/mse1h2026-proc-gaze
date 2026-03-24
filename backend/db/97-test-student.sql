8e7a62f08e46044b270572b906e163178d1c55f5
magofrays <vadimpozdeev2005@gmail.com>
2026-03-24 21:22:04 +0300

    inserting student

---
+++ backend/db/97-test-student.sql
@@ -0,0 +1,5 @@
+-- backend/db/97-test-student.sql
+INSERT INTO student(student_id, first_name, last_name, patronymic)
+VALUES
+  ('285a1389-be7c-43eb-9d76-9afbfb715458', 'Максим', 'Берлет', 'Валерьевич'),
+  ('1de37e3f-1a66-44a7-8bb3-adf1c5565dd5', 'Александр', 'Рудаков', 'Леонидович');
