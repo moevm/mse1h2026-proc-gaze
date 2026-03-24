2123cb80cae2db8d2840e8de8ff8b0c6e6d65689
AlexanderKamynin <kamyninsasa@gmail.com>
2026-03-25 00:38:04 +0300

    refactor: remove unnecessary

--- backend/db/97-test-student.sql
+++
@@ -1,5 +0,0 @@
--- backend/db/97-test-student.sql
-INSERT INTO student(student_id, first_name, last_name, patronymic, "group")
-VALUES
-  ('285a1389-be7c-43eb-9d76-9afbfb715458', 'Максим', 'Берлет', 'Валерьевич', '3385'),
-  ('1de37e3f-1a66-44a7-8bb3-adf1c5565dd5', 'Александр', 'Рудаков', 'Леонидович', '3384');
