598f806a4cef6f70576b8a74ec0109aea75bbde0
Alexundr38 <4sunwarrior1@mail.ru>
2026-04-02 12:29:09 +0300

    feat: add api for dump

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,4 @@
 export { uploadApi } from './modules/upload.api';
 export { recordingApi } from './modules/recording.api';
 export { mainApi } from './modules/main.api';
+export { dumpApi } from './modules/dump.api';
