d9705a19c087bcfc68ef3e0fd2e2d0bb926681ee
Alexundr38 <4sunwarrior1@mail.ru>
2026-04-20 21:43:56 +0300

    fix change dump parametres

--- frontend/src/api/modules/dump.api.ts
+++ frontend/src/api/modules/dump.api.ts
@@ -3,9 +3,9 @@ import { apiFileClient } from '../config'
 class DumpApi {
     async upload(file: File): Promise<any> {
         const formData = new FormData();
-        formData.append('file', file);
+        formData.append('student_dump', file);
 
-        const response = await apiFileClient.post('/dump', formData, {
+        const response = await apiFileClient.post('/students/dump', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
