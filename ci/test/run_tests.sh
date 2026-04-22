3185004d814e1e765cfc186994357da95dc76bdb
Kirill Gorskiy <kirillgorskiy@gmail.com>
2026-04-22 19:30:02 +0300

    activate venv on windows + change MODELS_URL

--- ci/test/run_tests.sh
+++ ci/test/run_tests.sh
@@ -10,7 +10,7 @@ if [ ! -d "$VENV_DIR" ]; then
     python3 -m venv "$VENV_DIR"
 fi
 
-source "$VENV_DIR/bin/activate"
+source "$VENV_DIR/bin/activate" || source "$VENV_DIR/Scripts/activate"
 
 pip install --upgrade pip
 pip install -r "$SCRIPT_DIR/requirements.txt"
