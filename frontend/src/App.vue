2652cf75b0d67cd508d4861178514b3ad4d77493
Alexundr38 <4sunwarrior1@mail.ru>
2026-04-24 15:36:47 +0300

    feat: add notification element in app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <NotificationToast
+      :notifications="pendingNotifications"
+      @remove="removeNotification"
+  />
   <router-view />
 </template>
 
+<script setup>
+import { onMounted } from 'vue';
+import { useNotifications } from '@/composables/useNotifications';
+import NotificationToast from '@/components/NotificationToast.vue';
+
+const { pendingNotifications, startPolling, removeNotification } = useNotifications();
+
+onMounted(() => {
+  startPolling(10000);
+});
+</script>
